8b60b11f80c3ec7d408d86d88dca7a8f0fd8ef87
Steffen Leußink <slk@fastbolt.com>
2023-07-14 09:32:12 +0200

    cs

--- src/EventSubscriber/EasterEggSubscriber.php
+++ src/EventSubscriber/EasterEggSubscriber.php
@@ -8,18 +8,18 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class EasterEggSubscriber implements EventSubscriberInterface
 {
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse',
+        ];
+    }
+
     public function onKernelResponse(ResponseEvent $event): void
     {
         $response = $event->getResponse();
 
         //will be on every response
         $response->headers->add(['EASTER_EGG_HEADER' => 'X_DEBUG_START_IS_TOO_LATE_HERE']);
-    }
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            KernelEvents::RESPONSE => 'onKernelResponse',
-        ];
     }
 }
